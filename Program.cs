2868ca036d556a78796bf09420e9a4bb9bf1fb45
Guicortiz <guilhermechenciortiz@gmail.com>
2021-10-25 21:12:04 -0300

    Make menu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,40 @@ namespace TextEditor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Want do you want do?");
+            Console.WriteLine("1 - Open file text");
+            Console.WriteLine("2 - Make new file text");
+            Console.WriteLine("----------------------");
+            Console.WriteLine("0 - Exit");
+
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: OpenFile(); break;
+                case 2: CreateFile(); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void OpenFile()
+        { }
+        static void CreateFile()
+        {
+            Console.Clear();
+            Console.WriteLine("Write your text (Esc to leave)");
+            Console.WriteLine("---------------");
+            string text = "";
+
+
+
         }
     }
 }
